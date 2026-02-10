2ce28861c43c57b58df5d201c41ef833dd10d881
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2026-02-10 14:06:14 +0000

    chore: reverts everything done until now

--- src/app/components/icons/Cart.tsx
+++
@@ -1,7 +0,0 @@
-export function CartIcon() {
-  return (
-    <span role="img" aria-hidden="true" className="text-lg">
-      🛒
-    </span>
-  );
-}
--- src/app/components/icons/Menu.tsx
+++
@@ -1,9 +0,0 @@
-export function MenuIcon() {
-  return (
-    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M2 5h14" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-      <path d="M2 9h10" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-      <path d="M2 13h14" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-    </svg>
-  );
-}
